ace4f831f8a358cdd894ea83db21fb3e65d024c6
achirar66 <achirar66@nu.ac.th>
2025-09-28 23:21:01 +0700

    first commit

--- lib/models/post.dart
+++ lib/models/post.dart
@@ -1,17 +1,19 @@
 
 // lib/post_model.dart
 class Post {
+  final int userId;
   final int id;
   final String title;
   final String body;
 
-  const Post({required this.id, required this.title, required this.body});
+  const Post({required this.id, required this.title, required this.body, required this.userId});
 
   factory Post.fromJson(Map<String, dynamic> json) {
     return Post(
       id: json['id'] as int,
       title: json['title'] as String,
       body: json['body'] as String,
+      userId: json['userId'] as int,
     );
   }
 }
